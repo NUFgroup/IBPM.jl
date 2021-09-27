143b633c9db7c7fb7fb75b8eb6d2f1a9f9c31cdf
Jared Callaham <jared.callaham@gmail.com>
2021-09-27 10:28:40 -0700

    Adding power iteration to Re=50 cylinder

---
+++ benchmarks/cyl50/run_stab.jl
@@ -0,0 +1,19 @@
+include("config.jl")  # Set up grid and other parameters
+
+# Load pre-computed DNS solution
+using FileIO
+data = load("dns_output.jld2")
+base_state = data["state"]
+
+# Construct the IBProblem corresponding to the DNS
+base_prob = ibpm.IBProblem(grid, cyls, Δt, Re, freestream=freestream);
+
+# Construct the linearized problem
+prob = ibpm.LinearizedIBProblem(base_state, base_prob, Δt)
+base_state, base_prob = nothing, nothing  # Free up memory
+
+ϵ = 1e-6   # Noise level for initial vorticity
+state = ibpm.IBState(prob, ϵ);
+
+using LinearMapss
+ibpm.advance!(state, prob, 0.0)
