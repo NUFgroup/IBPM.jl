cf1acbf9da6027c39ae0134e6d9119fae9a0e64d
Jared Callaham <jared.callaham@gmail.com>
2021-09-30 06:23:24 -0700

    Fixed Re=40 cyl benchmark

--- benchmarks/cyl_40.jl
+++ benchmarks/cyl_40.jl
@@ -1,12 +1,12 @@
 include("../src/ibpm.jl")
 
 # Define grid
-nx = 200  # num of x points on finest domain
-ny = 200  # num of y points on finest domain (length in y-dirn is len/m * n )
+xlims = (-1.0, 3.0)
+ylims = (-2.0, 2.0)
+boundary = (xlims..., ylims...) #left, right, bottom, and top of domain
 mg = 5   # num domains
-offx = 1.0; # offset in x dirn (on fine domain, x-grid runs from -offx to len-offx.
-offy = 2.0; # offset in y dirn (same as offx but in y-dirn)
-len = 4.0  # length of domain in x-direction
+Δx = 0.02
+grid = ibpm.make_grid(Δx, boundary, mg=mg)
 
 # Other parameters
 Re = 40.0
@@ -15,15 +15,29 @@ Re = 40.0
 Uinf = 1.0;   # Free-stream flow
 r = 0.5; # Cylinder radius
 
-grid = ibpm.make_grid(nx, ny, offx, offy, len, mg=mg)
 cyls = [ibpm.make_cylinder( r, grid.h, 0.0, 0.0 )]
 
-prob = ibpm.IBProblem(grid, cyls, Δt, Re, Uinf=Uinf, α=α);
+ #freestream conditions
+freestream = (Ux=t->1.0, Uy=t->0.0, inclination=t->0.0)
+
+function run_sim!(t, state, prob; output=1, callback=(state, prob)->nothing)
+	for i=1:length(t)
+		ibpm.advance!(state, prob, t[i])
+        if mod(i,output) == 0
+			callback(state, prob);  # Primitive callback, can be used for plotting or other output
+            @show (t[i], state.CD, state.CL, state.cfl)
+        end
+	end
+end
+
+prob = ibpm.IBProblem(grid, cyls, Δt, Re, freestream=freestream);
+
 state = ibpm.IBState(prob);
-T = 100
+T = 10
 t = 0:Δt:T
 
-ibpm.run_sim(t[1:2], state, prob) # Pre-compile
+run_sim!(t[1:2], state, prob) # Pre-compile
 
 # Advance to final time
-runtime = @elapsed ibpm.run_sim(t, state, prob; output=20)
+runtime = @elapsed run_sim!(t, state, prob; output=20)
+println(runtime)
